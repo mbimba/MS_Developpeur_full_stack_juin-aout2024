fb2e2cb85638eae2743a51ede781ece5acb22a69
MBIMBA Triomphe <mbimbatriomphe@gmail.com>
2024-07-16 15:25:04 +0200

    MIse en page de la page plats.php. POur éviter un décalage dû a la longueur de certains textes, jai centré le bloc.

--- Eval_MS_full_stack_lignerouge/plats.php
+++ Eval_MS_full_stack_lignerouge/plats.php
@@ -27,7 +27,7 @@ $categorie = getCategoryById($conn, $idCategorie);
   </div>
 </header>
 
-<main class="container-fluid mt-3 pt-5" >
+<main class="container-fluid mt-3 pt-5">
   <div class="row justify-content-center">
     <div class="col-md-8 col-lg-6">
       <h1 class="h5couleurcat text-center">LES PLATS: <?php echo $categorie['libelle']; ?></h1>
@@ -35,42 +35,46 @@ $categorie = getCategoryById($conn, $idCategorie);
   </div>
 
   <!-- CAROUSEL AVEC 12 IMAGES DE TOUTES LES PLATS -->
-  <div id="carouselExample" class="carousel slide plats-container" data-bs-ride="carousel">
-    <div class="carousel-inner">
-      <?php foreach($plats as $key => $plat) : ?>
-        <?php if($key % 4 === 0) : ?>
-          <div class="carousel-item <?php echo $key === 0 ? 'active' : ''; ?>">
-            <div class="card-group">
-        <?php endif; ?>
+  <div class="row justify-content-center">
+    <div class="col-md-10 col-lg-8">
+      <div id="carouselExample" class="carousel slide plats-container" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <?php foreach($plats as $key => $plat) : ?>
+            <?php if($key % 4 === 0) : ?>
+              <div class="carousel-item <?php echo $key === 0 ? 'active' : ''; ?>">
+                <div class="card-group">
+            <?php endif; ?>
 
-        <div class="col-sm-3 p-3 bg-dark text-white">
-          <img class="rounded-circle mx-auto d-block" src="Mesimages/<?php echo $plat['image']; ?>" alt="...">
-          <div class="card-body">
-            <h5 class="card-title tailletitreplat"><?php echo $plat['libelle']; ?></h5>
-            <p class="card-text tailledescriplat"><?php echo $plat['description']; ?></p>
-            <p class="card-text tailledescriplat">Prix : <?php echo $plat['prix']; ?> €</p>
-            <a href="commande.php?id=<?php echo $plat['id']; ?>" class="btn btn-primary tailletextcommand">Commander</a>
-          </div>
+            <div class="col-sm-3 p-3 bg-dark text-white">
+              <img class="rounded-circle mx-auto d-block" src="Mesimages/<?php echo $plat['image']; ?>" alt="...">
+              <div class="card-body">
+                <h5 class="card-title tailletitreplat"><?php echo $plat['libelle']; ?></h5>
+                <p class="card-text tailledescriplat"><?php echo $plat['description']; ?></p>
+                <p class="card-text tailledescriplat">Prix : <?php echo $plat['prix']; ?> €</p>
+                <a href="commande.php?id=<?php echo $plat['id']; ?>" class="btn btn-primary tailletextcommand">Commander</a>
+              </div>
+            </div>
+
+            <?php if(($key+1) % 4 === 0 || $key === count($plats)-1) : ?>
+                </div>
+              </div>
+            <?php endif; ?>
+
+          <?php endforeach; ?>
         </div>
 
-        <?php if(($key+1) % 4 === 0 || $key === count($plats)-1) : ?>
-            </div>
-          </div>
-        <?php endif; ?>
-
-      <?php endforeach; ?>
-    </div>
-
-    <!-- Boutons de carousel: j'y ai intégré du php pour masquer le bouton quand je clique sur une catégorie de plats de la page index.php -->
-    <div class="carousel-controls moncarousselcat">
-      <button class="carousel-control-prev moncarousselcatprev" id="carouselcatprec" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <?php if (!$hideButtons) { ?><button class="btn btn-secondary bg-dark rounded-4 col-sm-1 espacebouton" onclick="precedent()" type="button">Précédent</button>
-      </button>
-      <button class="carousel-control-next" id="carouselcatsuiv" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <button class="btn btn-secondary bg-dark rounded-4 col-sm-1 espacebouton" onclick="suivant()" type="button">Suivant</button><?php } ?>
-      </button>
+        <!-- Boutons de carousel: j'y ai intégré du php pour masquer le bouton quand je clique sur une catégorie de plats de la page index.php -->
+        <div class="carousel-controls moncarousselcat">
+          <button class="carousel-control-prev moncarousselcatprev" id="carouselcatprec" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <?php if (!$hideButtons) { ?><button class="btn btn-secondary bg-dark rounded-4 col-sm-1 espacebouton" onclick="precedent()" type="button">Précédent</button>
+          </button>
+          <button class="carousel-control-next" id="carouselcatsuiv" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <button class="btn btn-secondary bg-dark rounded-4 col-sm-1 espacebouton" onclick="suivant()" type="button">Suivant</button><?php } ?>
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </main>
